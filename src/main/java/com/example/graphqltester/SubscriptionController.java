e9ec1b3759a8bf8d243d7c69c062cc6c395ed842
calebcodesgud <calwoy1@gmail.com>
2022-08-02 17:57:53 -0400

    initial commit

--- src/main/java/com/example/graphqltester/SubscriptionController.java
+++ src/main/java/com/example/graphqltester/SubscriptionController.java
@@ -12,6 +12,7 @@ public class SubscriptionController {
 
     @SubscriptionMapping
     public Flux<Result> getFluxOfObjects(final DataFetchingEnvironment env) {
+        // used the data fetching env here because its also used in the actual query i first noticed this issue on
         return Flux.range(0, env.getSelectionSet().getImmediateFields().toString().length() ).map(Result::new).doOnSubscribe(sub ->
             log.info("getFluxOfObjects requested")
         ).doOnTerminate(() -> log.info("done"));
