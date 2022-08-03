452792ab4fb18e88c125e1cdc92c4fb39e4a7d8b
Brian Clozel <bclozel@vmware.com>
2022-08-03 11:26:30 +0200

    Rework GraphQL controllers
    Prior to this commit, the `getFluxOfObjects` query was not mapped nor
    used. This commit adds a new Greeting controller for testing a vanilla
    query.
    
    The subscription schema entry has been improved to take an argument
    setting the number of elements that should be streamed.

--- src/main/java/com/example/graphqltester/SubscriptionController.java
+++ src/main/java/com/example/graphqltester/SubscriptionController.java
@@ -1,21 +1,24 @@
 package com.example.graphqltester;
 
-import graphql.schema.DataFetchingEnvironment;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
 import org.springframework.stereotype.Controller;
-import reactor.core.publisher.Flux;
 
 @Slf4j
 @Controller
 public class SubscriptionController {
 
-    @SubscriptionMapping
-    public Flux<Result> getFluxOfObjects(final DataFetchingEnvironment env) {
-        // used the data fetching env here because its also used in the actual query i first noticed this issue on
-        return Flux.range(0, env.getSelectionSet().getImmediateFields().toString().length() ).map(Result::new).doOnSubscribe(sub ->
-            log.info("getFluxOfObjects requested")
-        ).doOnTerminate(() -> log.info("done"));
-    }
+	@SubscriptionMapping
+	public Flux<Result> getFluxOfObjects(@Argument Integer count) {
+		return Flux.range(0, count).map(Result::new)
+				.doOnSubscribe(sub ->
+						log.info("getFluxOfObjects requested")
+				)
+				.doOnNext(value -> log.info("Sending value: " + value))
+				.doOnTerminate(() -> log.info("done"));
+	}
 
 }
